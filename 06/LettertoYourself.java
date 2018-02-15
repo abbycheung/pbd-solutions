3e21781587e5b974e81050bf089cda9b8d98d01b
abbycheung <abby.cheung18@ycdsbk12.ca>
2018-02-15 09:35:17 -0500

    upload 05, 06

---
+++ 06/LettertoYourself.java
@@ -0,0 +1,19 @@
+public class LettertoYourself
+{
+	public static void main ( String[] args)
+	{ 
+
+		System.out.println("+---------------------------------------------------------+");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                    #### |");
+		System.out.println("|                                                         |");
+		System.out.println("|                                                         |");
+		System.out.println("|                              Bill Gates                 |");
+		System.out.println("|                              1 Microsoft Way            |");
+		System.out.println("|                              Redmond, WA 98104          |");
+		System.out.println("|                                                         |");
+		System.out.println("+---------------------------------------------------------+");
+	}
+}
+		
