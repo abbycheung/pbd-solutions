500942e1fe4f00d6d53f5f678025ce806631705f
abbycheung <abby.cheung18@ycdsbk12.ca>
2018-02-15 09:43:44 -0500

    Add files via upload

---
+++ 07/YourInitials.java
@@ -0,0 +1,14 @@
+public class YourInitials 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("  A    CCC ");
+		System.out.println(" A A  C    C");
+		System.out.println("A   A C    ");
+		System.out.println("AAAAA C");
+		System.out.println("A   A C");
+		System.out.println("A   A C    C");   
+		System.out.println("A   A  CCC ");
+	}
+}
+	
