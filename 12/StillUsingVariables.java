7469f442d5c8bfe78212a32d7456d14ee5789a48
abbycheung <abby.cheung18@ycdsbk12.ca>
2018-02-16 09:25:42 -0500

    Add files via upload

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,9 @@
+public class StillUsingVariables {
+	public static void main(String[] args) {
+		String name = "Abby Cheung";
+		int gradYear = 2018;
+
+		System.out.println("My name is " + name + " I'll graduate in " + gradYear);
+		
+	}
+}
